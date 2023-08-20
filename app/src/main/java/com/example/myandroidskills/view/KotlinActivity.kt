fa9cc53404ba339508a3480358f56799549e9234
nikhilsinghdev <vishal@spiraldevapps.com>
2023-08-20 18:27:53 +0530

    Multiple GitHub Account in Kotlin

--- app/src/main/java/com/example/myandroidskills/view/KotlinActivity.kt
+++ app/src/main/java/com/example/myandroidskills/view/KotlinActivity.kt
@@ -2,11 +2,15 @@ package com.example.myandroidskills.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myandroidskills.R
 
 class KotlinActivity : AppCompatActivity() {
+    val TAG = this.javaClass.name
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
+        Log.d(TAG,"Checking Commit After Multiple GitHub Account in Kotlin")
+
     }
 }
